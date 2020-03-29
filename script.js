7134c9b6e9b6e13b88accc9a702ffd950ab912b6
Anna McDougall <anna.mcdougall@gmail.com>
2020-03-29 16:33:49 +0200

    Added reset dimensions button functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /// Define variables
-
+let newGridDimension
 
 /// DOM elements
 
@@ -8,13 +8,22 @@ const resetButton = document.querySelector("#clearButton");
 
 
 /// Functions
-function createGridDivs() {
+function createOriginalGrid() {
     for (let x = 0; x<256; x++) {
         let newDiv = document.createElement('div');
         newDiv.setAttribute("id", x)
         newDiv.setAttribute("class", "gridSquare");
         container.appendChild(newDiv);
         
+    }
+}
+
+function populateNewGrid(num) {
+    for (let x = 0; x<(num*num); x++) {
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute("id", x)
+        newDiv.setAttribute("class", "gridSquare");
+        container.appendChild(newDiv);
     }
 }
 
@@ -29,9 +38,20 @@ resetButton.addEventListener("click", () => {
     var squares = [].slice.call(container.children);
     squares.forEach(function(e){
         e.style.backgroundColor = "white"});
+     newGridDimension = window.prompt("How many columns and rows would you like for your new grid?");
+     newGridDimension = parseInt(newGridDimension);
+     if (typeof newGridDimension === "number") { 
+        squares.forEach(function(e){
+            e.remove()});
+        document.getElementById("container").style.gridTemplateColumns = `repeat(${newGridDimension}, 1fr)`;
+        document.getElementById("container").style.gridTemplateRows = `repeat(${newGridDimension}, 1fr)`;
+    populateNewGrid(newGridDimension);
+    } 
 })
 
 /// Call events if needed
-createGridDivs();
+
+createOriginalGrid();
+
 
 
