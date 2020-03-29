6aac1880d4af6886f3bb63510481cb7570e52c6f
Anna McDougall <anna.mcdougall@gmail.com>
2020-03-29 14:28:52 +0200

    Got grid and reset function working

--- script.js
+++ script.js
@@ -3,12 +3,35 @@
 
 /// DOM elements
 
-const container = document.getElementsByClassName("container");
+const container = document.querySelector(".container");
+const resetButton = document.querySelector("#clearButton");
 
 
 /// Functions
-
+function createGridDivs() {
+    for (let x = 0; x<256; x++) {
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute("id", x)
+        newDiv.setAttribute("class", "gridSquare");
+        container.appendChild(newDiv);
+        
+    }
+}
 
 /// Event listeners
+
+document.getElementById("container").addEventListener("mouseover", function(event) {
+    var target = event.target;
+    target.style.backgroundColor = "black";
+  }, false);
+
+resetButton.addEventListener("click", () => {
+    var squares = [].slice.call(container.children);
+    squares.forEach(function(e){
+        e.style.backgroundColor = "white"});
+})
+
+/// Call events if needed
+createGridDivs();
 
 
